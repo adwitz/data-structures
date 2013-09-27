1129b0ec232f7ab75ae86bc74e40adbbc12c19d8
adwitz <adam.witzel@gmail.com>
2013-09-27 16:00:28 -0700

    doubly linked; added addToHead, removeTail

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -11,13 +11,38 @@ var makeLinkedList = function(){
       list.tail = newNode;
     } else {
       list.tail.next = newNode;
+      newNode.prev = list.tail;
       list.tail = newNode;
+    }
+  };
+
+  list.addToHead = function(value){
+    var newNode = makeNode(value);
+    if (!list.head){
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.head.prev = newNode;
+      newNode.next = list.head;
+      list.head = newNode;
     }
   };
 
   list.removeHead = function(){
     if (list.head) {
+      var oldHead = list.head.value;
       list.head = list.head.next;
+      list.head.prev = null;
+      return oldHead;
+    }
+  };
+
+  list.removeTail = function(){
+    if (list.tail){
+      var oldTail = list.tail.value;
+      list.tail = list.tail.prev;
+      list.tail.next = null;
+      return oldTail;
     }
   };
 
@@ -32,6 +57,8 @@ var makeLinkedList = function(){
     return false;
   };
 
+
+
   return list;
 };
 
@@ -39,6 +66,7 @@ var makeNode = function(value){
   var node = {};
   node.value = value;
   node.next = null;
+  node.prev = null;
 
   return node;
 };
