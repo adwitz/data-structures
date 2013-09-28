f4f3b0a85fcc192b78bff819a1b6621b1cad88e7
adwitz <adam.witzel@gmail.com>
2013-09-28 11:17:07 -0700

    add test for depthFirstLog

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -20,6 +20,30 @@ describe("binarySearchTree", function() {
     binarySearchTree.insert(60);
     expect(binarySearchTree.contains(20)).toEqual(false);
   });
+
+  it("should return false if value does not exist", function(){
+    binarySearchTree.insert(88);
+    binarySearchTree.insert(99);
+    binarySearchTree.insert(101);
+    expect(binarySearchTree.contains(101)).toEqual(true);
+  });
+
+  it("should change parent value to product of parent and children", function(){
+    binarySearchTree.insert(88);
+    binarySearchTree.insert(99);
+    binarySearchTree.insert(101);
+    binarySearchTree.insert(98);
+    var doub = function(){
+      if (this.left){
+        this.value = this.value * this.left.value;
+      }
+      if (this.right){
+        this.value = this.value * this.right.value;
+      }
+    };
+    binarySearchTree.depthFirstLog(doub);
+    expect(binarySearchTree.contains(99 * 101 * 98)).toEqual(true);
+  });
   // add more tests here to test the functionality of binarySearchTree
 });
 
